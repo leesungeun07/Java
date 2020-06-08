189bc07755799c525b4bcce11e339d63ccd90a84
leesungeun07 <63436892+leesungeun07@users.noreply.github.com>
2020-06-08 09:25:01 +0900

    [문제1] 1부터 10까지의 숫자를 출력하고 합계도 출력

---
+++ Java_0608_30214_이성은.java
@@ -0,0 +1,12 @@
+
+public class _0608_30214_¿Ãº∫¿∫ {
+
+	public static void main(String[] args) {
+		int res = 0;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			res += i;
+		}
+		System.out.println(res);
+	}
+}
