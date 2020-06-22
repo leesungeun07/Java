a6f824807fc8a3be56333d46732f4d7d2cfb374c
leesungeun07 <63436892+leesungeun07@users.noreply.github.com>
2020-06-22 09:32:30 +0900

    학습지_S_3 [문제2]

---
+++ Java_200622_30214_이성은.java
@@ -0,0 +1,17 @@
+package a;
+
+import java.util.Scanner;
+public class _1 {
+
+	public static void main(String[] args) {
+		
+		//@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+
+		System.out.printf("%d", a + b);
+	}
+
+}
